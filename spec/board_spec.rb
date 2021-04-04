a6aa76fef470c0e789fde422aef0bc773d19178b
Dane Brophy <heartbeat@Danes-MacBook-Pro.local>
2021-04-04 15:25:34 -0600

    Complete valid placement? method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -33,7 +33,6 @@ describe 'Board' do
 
      expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
      expect(board.valid_placement?(submarine, ["A1", "A2", "A3"])).to eq(false)
-     expect(board.valid_placement?(cruiser, ["A1", "A2", "A3"])).to eq(true)
     end
 
     it 'can test if the placement coordinates are consecutive' do
@@ -45,7 +44,6 @@ describe 'Board' do
       expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
       expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
       expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
-      expect(board.valid_placement?(submarine, ["A1", "A2"])).to eq(true)
     end
 
     it 'can test if the placement coordinates are not diagonal' do
@@ -55,6 +53,15 @@ describe 'Board' do
 
       expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to eq(false)
       expect(board.valid_placement?(submarine, ["C2", "D3"])).to eq(false)
+    end
+
+    it 'passes all of the previous tests for valid placement' do
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)
+
+      expect(board.valid_placement?(submarine, ["A1", "A2"])).to eq(true)
+      expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to eq(true)
     end
   end
 end
